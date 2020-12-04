643bd6ae0196472b088529c77db5526377b51b64
kkasperek <kt.kasperek@gmail.com>
2020-12-04 15:17:16 -0600

    initial commit

---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-undef */
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+// images references in the manifest
+import "../../assets/icon-16.png";
+import "../../assets/icon-32.png";
+import "../../assets/icon-80.png";
+
+/* global document, Office, Word */
+
+Office.onReady(info => {
+  if (info.host === Office.HostType.OneNote) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("run").onclick = run;
+  }
+});
+
+export async function run() {
+  try {
+    await OneNote.run(async context => {
+
+        // Get the current page.
+        var page = context.application.getActivePage();
+
+        // Queue a command to set the page title.
+        page.title = "Hello World";
+
+        // Queue a command to add an outline to the page.
+        var html = "<p><ol><li>Item #1</li><li>Item #2</li></ol></p>";
+        page.addOutline(40, 90, html);
+
+        // Run the queued commands, and return a promise to indicate task completion.
+        return context.sync();
+    });
+} catch (error) {
+    console.log("Error: " + error);
+}
+  return OneNote.run(async context => {
+    /**
+     * Insert your Word code here
+     */
+
+    // insert a paragraph at the end of the document.
+    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+
+    // change the paragraph color to blue.
+    paragraph.font.color = "blue";
+
+    await context.sync();
+  });
+}
